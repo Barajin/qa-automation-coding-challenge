fbfc1bc95148cb90ac373ae051c51d5c6539234e
Jesus Barajas <jesus.barajas@ordermygear.com>
2024-04-09 13:18:52 -0700

    Added fixtures to the scripts

--- cypress/e2e/testGithubReposSearch.cy.js
+++ cypress/e2e/testGithubReposSearch.cy.js
@@ -5,7 +5,14 @@ describe('Test 01: End to end Positive Scenario: Succesful search', () => {
   it('Should search and validate search', () => {
     cy.visit('http://localhost:3000')
     cy.get('h1').should('have.text', 'Get Github Repos') // Verify that the header of the page
-    cy.get('#username').type('Test') // Input text 
+    cy.fixture('testdata.json').then((data) => {
+      // Extract the value you want to type from the JSON object
+      const valueToType = data.PositiveScenarioText;
+      
+      // Type the value into an input field
+      cy.get('#username').type(valueToType);
+    });
+
     cy.get('.submit').click()
     cy.get('strong').should('have.text', 'Success!') // Success message validation
     cy.get('ul > :nth-child(1) > :nth-child(1)').should('be.visible')// Verify that a least one element is displayed
--- cypress/e2e/testGithubReposSearchNegativeScenario.cy.js
+++ cypress/e2e/testGithubReposSearchNegativeScenario.cy.js
@@ -4,9 +4,14 @@ describe('Test 02: End to end Negative Scenario. No results.', () => {
   it('Should search and validate search', () => {
     cy.visit('http://localhost:3000')
     cy.get('h1').should('have.text', 'Get Github Repos') // Verify that the header of the page
-    cy.get('#username').type('what is this') // Input text 
+    cy.fixture('testdata.json').then((data) => {
+      // Extract the value you want to type from the JSON object
+      const valueToType = data.NegativeScenarioText;
+      
+      // Type the value into an input field
+      cy.get('#username').type(valueToType);
+    });
     cy.get('.submit').click()
-    //cy.get('.message-failure').should('have.text', 'Github user not found!') // No user found message validation
     //Now lets validate the Result
     cy.ValidateText('.output-status-text','No repos') // Custom Command to validate text of results 
 
